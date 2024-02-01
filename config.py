a70c369efc64ccee6087edbdf67a0772693196d7
carlsomeHand <123858947+carlsomeHand@users.noreply.github.com>
2024-02-01 17:47:29 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 import os
+import json
 
 # 获取当前脚本文件的绝对路径
-corpus_path = os.path.split(os.path.abspath(__file__))[0]
+prefix_path = os.path.split(os.path.abspath(__file__))[0]
 
-CORPUS = os.path.join(corpus_path, 'Sam_Altman.txt')
+CORPUS = os.path.join(prefix_path, 'Sam_Altman.txt')
+api_json = os.path.join(prefix_path, 'api.json')
+
+with open(api_json, 'r', encoding='utf-8') as f:
+     api_key = json.load(f)['api_key']
 
 # api_key = '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'
