690ff1ab39d32a71108a90d0e5a260da245c0c12
carlsomeHand <123858947+carlsomeHand@users.noreply.github.com>
2024-02-01 09:11:23 +0000

    修改corpus路径

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
-CORPUS = './Sam Altman-preprocessed.txt'
+import os
+
+# 获取当前脚本文件的绝对路径
+corpus_path = os.path.split(os.path.abspath(__file__))[0]
+
+CORPUS = os.path.join(corpus_path, './Sam Altman-preprocessed.txt')
 
 api_key = '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'
