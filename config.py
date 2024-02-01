16fee0c8f94e448d50ad3618b939ccbd22036f10
carlsomeHand <123858947+carlsomeHand@users.noreply.github.com>
2024-02-01 08:53:05 +0000

    提交代码

---
+++ config.py
@@ -0,0 +1,3 @@
+CORPUS = './Sam Altman-preprocessed.txt'
+
+api_key = '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'
---
+++ main.py
@@ -0,0 +1,55 @@
+import openai
+import streamlit as st
+
+from config import CORPUS, api_key
+
+with open(CORPUS, 'r', encoding='utf-8') as f:
+    corpus = f.read()
+
+
+def run():
+    st.set_page_config(
+        page_title="Hello",
+        page_icon="👋",
+    )
+
+    try:
+        openai.api_key = api_key
+    except ImportError:
+        raise ValueError(
+            "Could not import openai python package. "
+            "Please install it with `pip install openai`."
+        )
+
+    query = st.text_input('You can ask anything about Sam Altman(according to Wikipedia)', '')
+
+    prompt = f"According to the text from Wikipedia, please answer the question below.\n\n" \
+             f"Q: {query}\n\n" \
+             f"Wikipedia text: {corpus}"
+
+    completion = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature=0.7,
+        stream=True
+    )
+
+    """
+    completion = openai.ChatCompletion.create(
+        model="text-davinci-002",
+        messages=messages,
+        stream=True
+    )
+    """
+
+    answer_text = ""
+
+    for chunk in completion:
+        if hasattr(chunk.choices[0].delta, "content"):
+            answer_text = "".join([answer_text, chunk.choices[0].delta.content])
+            st.write(answer_text)
+
+
+if __name__ == "__main__":
+    run()
+    
