165ee0d747596d1e4e1099ec9d01f89b8fd3e9f0
carlsomeHand <123858947+carlsomeHand@users.noreply.github.com>
2024-02-02 11:43:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ def run():
         page_title="Hello",
         page_icon="👋",
     )
-    LOGGER.info(f"api_key:{api_key}")
+    # LOGGER.info(f"api_key:{api_key}")
     client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
     
     query = st.text_input('You can ask anything about Sam Altman(according to Wikipedia)', 'Hello.')
