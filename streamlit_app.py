a2fc8ca42188d865faec646e3e164ceb1db79689
carlsomeHand <123858947+carlsomeHand@users.noreply.github.com>
2024-02-02 14:14:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,50 +5,50 @@ from streamlit.logger import get_logger
 
 from config import CORPUS
 
-
 LOGGER = get_logger(__name__)
 
 with open(CORPUS, 'r', encoding='utf-8') as f:
     corpus = f.read()
 
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
 # LOGGER.info(f'corpus: {CORPUS}')
 
+
 def run():
+    def text_generator():
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+            model="gpt-3.5-turbo",
+            stream=True,
+        )
+
+        for chunk in chat_completion:
+            if hasattr(chunk.choices[0].delta, "content"):
+                if type(chunk.choices[0].delta.content) is str:
+                    yield chunk.choices[0].delta.content
+
     st.set_page_config(
         page_title="Hello",
         page_icon="👋",
     )
     # LOGGER.info(f"api_key:{api_key}")
-    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-    
-    query = st.text_input('You can ask anything about Sam Altman(according to Wikipedia)', 'Who is Sam Altman?')
 
+    query = st.text_input('You can ask anything about Sam Altman(according to Wikipedia)', 'Who is Sam Altman?')
 
     prompt = f"According to the text from Wikipedia, please answer the question below.\n\n" \
              f"Q: {query}\n\n" \
              f"Wikipedia text: {corpus}"
 
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": prompt,
-            }
-        ],
-        model="gpt-3.5-turbo",
-        stream=True,
-    )
+    if st.button("Answer"):
+        st.write_stream(text_generator)
 
-    answer_text = ""
-
-    for chunk in chat_completion:
-        if hasattr(chunk.choices[0].delta, "content"):
-            answer_text = "".join([answer_text, chunk.choices[0].delta.content])
-            st.write(answer_text)
-            
-    query.empty()
-    st.button("Re-run")
 
 if __name__ == "__main__":
     run()
-    
+
