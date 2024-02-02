c3087a9c094b4a53d280c3f0de52dfe9622b9d76
carlsomeHand <123858947+carlsomeHand@users.noreply.github.com>
2024-02-02 14:52:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,6 @@ with open(CORPUS, 'r', encoding='utf-8') as f:
 
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
-# LOGGER.info(f'corpus: {CORPUS}')
-
 
 def run():
     def text_generator():
@@ -28,26 +26,46 @@ def run():
             stream=True,
         )
 
+        full_answer = ""
+
         for chunk in chat_completion:
             if hasattr(chunk.choices[0].delta, "content"):
                 character = chunk.choices[0].delta.content
                 if type(character) is str and len(character) > 0:
+                    full_answer = "".join(full_answer, character)
                     yield character
+
+        LOGGER.info(f'answer: {full_answer}')
 
     st.set_page_config(
         page_title="Hello",
         page_icon="👋",
     )
-    # LOGGER.info(f"api_key:{api_key}")
 
-    query = st.text_input('You can ask anything about Sam Altman(according to Wikipedia)', 'Who is Sam Altman?')
+    st.title("Welcome to the Chatbot👋")
+
+    # 文本输入
+    query = st.text_input('You can ask anything about Sam Altman(according to Wikipedia) by Chatgpt',
+                          'Who is Sam Altman?')
 
     prompt = f"According to the text from Wikipedia, please answer the question below.\n\n" \
              f"Q: {query}\n\n" \
              f"Wikipedia text: {corpus}"
 
+    # 通过点击按钮，调用生成器形成流式输出
     if st.button("Answer"):
+        LOGGER.info(f'user query: {query}')
         st.write_stream(text_generator)
+
+        st.write("Is this conversation helpful so far?")
+        if st.button("Good"):
+            feedback = "Good"
+            st.write("Thank you!😊")
+        if st.button("Bad"):
+            st.write("Sorry!😔")
+            feedback = "Bad"
+
+    LOGGER.info(f'user feedback: {feedback}')
 
 
 if __name__ == "__main__":
