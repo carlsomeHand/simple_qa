b8ef495fc69cf25c682f4390f2ce6c99f25b0ba6
carlsomeHand <123858947+carlsomeHand@users.noreply.github.com>
2024-02-03 09:54:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,8 +60,10 @@ def run():
         st.write("Is this conversation helpful so far?")
         if st.button("Good"):
             LOGGER.info('user feedback: Good')
+            st.rerun()
         if st.button("Bad"):
             LOGGER.info('user feedback: Bad')
+            st.rerun()
 
 
 if __name__ == "__main__":
